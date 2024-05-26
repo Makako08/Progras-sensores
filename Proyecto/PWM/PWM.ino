731915b0f7d49986ce954664859cabf1fb9a59a1
Makako08 <52816894+Makako08@users.noreply.github.com>
2024-05-25 20:44:04 -0600

    Update PWM.ino

--- Proyecto/PWM/PWM.ino
+++ Proyecto/PWM/PWM.ino
@@ -1,12 +1,29 @@
 led = 9;
 sw = 2;
+pot = A0;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+  pinMode(sw, INPUT);
+  pinMode(pot, INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (digitalRead(sw) == 1){
+    for (int i = 0; i < 255; i++)
+    {
+      analogWrite(led, i);
+      delay(30);
+    }
+    for (int i = 255; i > 0 ; i--)
+    {
+      analogWrite(led, i);
+      delay(30);
+    }
+  }
 
+  while (digitalRead(sw) == 0){
+    analogWrite(led, analogRead(pot));
+  }
 }
